65734d8e57f9c8d04fb7b66c6e14bf084e2ecc2b
Antoni Casas i Muñoz <acasas@essi.upc.edu>
2019-06-04 12:37:46 +0200

    Improves documentation

--- src/main/java/upc/stakeholdersrecommender/controller/StakeholdersRecommenderController.java
+++ src/main/java/upc/stakeholdersrecommender/controller/StakeholdersRecommenderController.java
@@ -38,7 +38,7 @@ public class StakeholdersRecommenderController {
             " or not. All information in the database is purged every time this method is called. A person's relation to the project is defined with" +
             "PARTICIPANT (availability is expressed in hours), while the person is defined in PERSONS, the requirements in REQUIREMENTS, the project in PROJECTS, and a person's" +
             "relation to a requirement in RESPONSIBLES.", notes = "", response = BatchReturnSchema.class)
-    public ResponseEntity<BatchReturnSchema> addBatch(@RequestBody BatchSchema batch, @ApiParam(value = "Whether the recommendation take into account the stakeholder's availability or not.", example = "false")
+    public ResponseEntity<BatchReturnSchema> addBatch(@RequestBody BatchSchema batch, @ApiParam(value = "Whether the recommendation take into account the stakeholder's availability or not.", example = "false", required=true)
     @RequestParam Boolean withAvailability) throws Exception {
         int res = 0;
         try {
@@ -53,7 +53,7 @@ public class StakeholdersRecommenderController {
     @RequestMapping(value = "reject_recommendation", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation(value = "Recommendation rejection method: used to state that the user identified by REJECTED must not be assigned to REQUIREMENT. The" +
             " rejection is performed by USER.", notes = "")
-    public ResponseEntity recommend_reject(@ApiParam(value = "Person who is rejected.", example = "Not JohnDoe")@RequestParam("rejected") String rejected,@ApiParam(value = "Person who rejects.", example = "JohnDoe") @RequestParam("user") String user,@ApiParam(value = "From which requirement is the person rejected.", example = "1") @RequestParam("requirement") String requirement) {
+    public ResponseEntity recommend_reject(@ApiParam(value = "Person who is rejected.", example = "Not JohnDoe", required=true)@RequestParam("rejected") String rejected,@ApiParam(value = "Person who rejects.", example = "JohnDoe", required=true) @RequestParam("user") String user,@ApiParam(value = "From which requirement is the person rejected.", example = "1", required=true) @RequestParam("requirement") String requirement) {
         stakeholdersRecommenderService.recommend_reject(rejected, user, requirement);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
@@ -64,14 +64,14 @@ public class StakeholdersRecommenderController {
             "\n The parameter projectSpecific specifies if the recommendation takes into account all stakeholders given in the batch_process, or only those" +
             " specified in \"PARTICIPANTS\", in the batch_process", notes = "", response = RecommendReturnSchema[].class)
     public ResponseEntity<List<RecommendReturnSchema>> recommend(@RequestBody RecommendSchema request,
-                                                                 @ApiParam(value = "Returns the top k stakeholders.", example = "10")@RequestParam Integer k,@ApiParam(value = "Considers stakeholders from all projects or only from one.", example = "false") @RequestParam Boolean projectSpecific) throws Exception {
+                                                                 @ApiParam(value = "Returns the top k stakeholders.", example = "10",required=true)@RequestParam Integer k,@ApiParam(value = "Considers stakeholders from all projects or only from one.", example = "false") @RequestParam Boolean projectSpecific) throws Exception {
         List<RecommendReturnSchema> ret = stakeholdersRecommenderService.recommend(request, k, projectSpecific);
         return new ResponseEntity<List<RecommendReturnSchema>>(ret, HttpStatus.CREATED);
     }
 
     @RequestMapping(value = "setEffort", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation(value = "Set the mapping of effort points into hours, the effort points go in a scale from 1 to 5, the effort is specific to a project", notes = "")
-    public ResponseEntity setEffort(@RequestBody SetEffortSchema eff,@ApiParam(value = "Which project this effort is refering to.", example = "1") @RequestParam String project) throws IOException {
+    public ResponseEntity setEffort(@RequestBody SetEffortSchema eff,@ApiParam(value = "Which project this effort is refering to.", example = "1",required=true) @RequestParam String project) throws IOException {
         effortCalc.setEffort(eff, project);
         return new ResponseEntity(HttpStatus.CREATED);
     }
@@ -80,7 +80,7 @@ public class StakeholdersRecommenderController {
     @ApiOperation(value = "Generate a mapping of effort points into hours specific to the project specified, based in the historic information given" +
             ",a list of hours per effort point, based on the time a requirement with those effort points required to be finished. The effort points go" +
             "in a scale from 1 to 5", notes = "")
-    public ResponseEntity calculateEffort(@RequestBody EffortCalculatorSchema eff,@ApiParam(value = "Which project this effort is refering to.", example = "1") @RequestParam String project) throws IOException {
+    public ResponseEntity calculateEffort(@RequestBody EffortCalculatorSchema eff,@ApiParam(value = "Which project this effort is refering to.", example = "1",required=true) @RequestParam String project) throws IOException {
         effortCalc.effortCalc(eff, project);
         return new ResponseEntity(HttpStatus.CREATED);
     }
