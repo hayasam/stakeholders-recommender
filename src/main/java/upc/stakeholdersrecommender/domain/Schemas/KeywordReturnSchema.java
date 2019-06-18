a40398ec0a5b8a529b20317e105d462b45a5b962
Antoni Casas <tonicasas6@gmail.com>
2019-06-19 00:11:14 +0200

    Adds possibility of returning requirements with their keywords in batch process

---
+++ src/main/java/upc/stakeholdersrecommender/domain/Schemas/KeywordReturnSchema.java
@@ -0,0 +1,30 @@
+package upc.stakeholdersrecommender.domain.Schemas;
+
+import java.util.List;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description = "Class representing a requirement with its keywords.")
+public class KeywordReturnSchema {
+    @ApiModelProperty(notes = "ID of the requirement.", example = "1", required = true)
+    private String requirement;
+    @ApiModelProperty(notes = "Set of skills of the requirement.", example = "[ubuntu,UI,linux]", required = true)
+    private List<String> skills;
+
+    public String getRequirement() {
+        return requirement;
+    }
+
+    public void setRequirement(String requirement) {
+        this.requirement = requirement;
+    }
+
+    public List<String> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<String> skills) {
+        this.skills = skills;
+    }
+
+}
---
+++ src/main/java/upc/stakeholdersrecommender/domain/Schemas/ProjectKeywordSchema.java
@@ -0,0 +1,29 @@
+package upc.stakeholdersrecommender.domain.Schemas;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.util.List;
+@ApiModel(description = "Class representing a project with its list of requirements with their keywords.")
+public class ProjectKeywordSchema {
+    @ApiModelProperty(notes = "Requirements with their keywords.", required = true)
+    List<KeywordReturnSchema> requirements;
+    @ApiModelProperty(notes = "ID of the project.", example = "1", required = true)
+    String projectId;
+
+    public List<KeywordReturnSchema> getRequirements() {
+        return requirements;
+    }
+
+    public void setRequirements(List<KeywordReturnSchema> requirements) {
+        this.requirements = requirements;
+    }
+
+    public String getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(String projectId) {
+        this.projectId = projectId;
+    }
+}
