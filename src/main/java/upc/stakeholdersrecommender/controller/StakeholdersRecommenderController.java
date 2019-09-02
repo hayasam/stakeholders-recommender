c6be8b61275114baf5a4371daca3258dc695ad60
Antoni Casas <tonicasas6@gmail.com>
2019-09-02 12:01:39 +0200

    Improves logging

--- src/main/java/upc/stakeholdersrecommender/controller/StakeholdersRecommenderController.java
+++ src/main/java/upc/stakeholdersrecommender/controller/StakeholdersRecommenderController.java
@@ -15,6 +15,7 @@ import upc.stakeholdersrecommender.service.EffortCalculator;
 import upc.stakeholdersrecommender.service.StakeholdersRecommenderService;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 
@@ -39,19 +40,19 @@ public class StakeholdersRecommenderController {
                                    @RequestParam Boolean autoMapping, @ApiParam(value = "If set to true, the endpoint returns each requirement with its set of keywords.", example = "true", required = true)
                                    @RequestParam Boolean keywords, @ApiParam(value = "Whether bugzilla preprocessing is used", example = "true", required = false) @RequestParam(value = "keywordPreprocessing", defaultValue = "false", required = false) Boolean bugzilla,
                                    @ApiParam(value = "Whether OpenReq Live logging is taken into account", example = "false", required = false) @RequestParam(value = "logging", defaultValue = "false", required = false) Boolean logging) throws Exception {
-        System.out.println("Starting batch process");
+        System.out.println("Starting batch process from "+organization+" | "+new Date());
         int res = 0;
         try {
             res = stakeholdersRecommenderService.addBatch(batch, withAvailability, withComponent, organization, autoMapping, bugzilla, logging,0);
         } catch (IOException e) {
-            System.out.println("Finished batch process");
+            System.out.println("Finished batch process "+organization+" | "+new Date());
             return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
         if (!keywords)
             return new ResponseEntity<>(new BatchReturnSchema(res), HttpStatus.CREATED);
         else {
             List<ProjectKeywordSchema> keys = stakeholdersRecommenderService.extractKeywords(organization, batch);
-            System.out.println("Finished batch process");
+            System.out.println("Finished batch process "+organization+" | "+new Date());
             return new ResponseEntity<>(keys, HttpStatus.CREATED);
         }
     }
@@ -60,9 +61,9 @@ public class StakeholdersRecommenderController {
     @ApiOperation(value = "This endpoint is used to state that the user identied by REJECTED must not be recommended for REQUIREMENT if USER performs the recommendation for REQUIREMENT.", notes = "")
     public ResponseEntity recommendReject(@ApiParam(value = "Id of the person who is rejected.", example = "Not JohnDoe", required = true) @RequestParam("rejected") String rejected, @ApiParam(value = "Id of the person who makes the rejection.", example = "JohnDoe", required = true) @RequestParam("user") String user, @ApiParam(value = "Id of the requirement from which the person REJECTED is rejected.", example = "1", required = true) @RequestParam("requirement") String requirement
             , @ApiParam(value = "The organization that is making the request.", example = "UPC", required = true) @RequestParam String organization) {
-        System.out.println("Starting rejection of recommendation");
+        System.out.println("Starting rejection of recommendation from "+organization+" | "+new Date());
         stakeholdersRecommenderService.recommend_reject(rejected, user, requirement, organization);
-        System.out.println("Finished rejection of recommendation");
+        System.out.println("Finished rejection of recommendation from "+organization+" | "+new Date());
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
@@ -71,9 +72,9 @@ public class StakeholdersRecommenderController {
     public ResponseEntity<List<RecommendReturnSchema>> recommend(@RequestBody RecommendSchema request,
                                                                  @ApiParam(value = "Maximum number of stakeholders to be returned by the recommender.", example = "10", required = true) @RequestParam Integer k, @ApiParam(value = "If set to true, the recommendation only takes into account as possible set of stakeholders the ones in the project to which the requirement pertains. If set to false, this set of stakeholders will be all the stakeholders received in the batch_process of the organization that is making the request, and will take all stakeholders with enough availability in any project. The availabilityScore of the participants of other projects will be always one if they are considered. ", required = true, example = "false") @RequestParam Boolean projectSpecific
             , @ApiParam(value = "The organization that is making the request.", example = "UPC", required = true) @RequestParam String organization) throws Exception {
-        System.out.println("Starting recommendation");
+        System.out.println("Starting recommendation from "+organization+" | "+new Date());
         List<RecommendReturnSchema> ret = stakeholdersRecommenderService.recommend(request, k, projectSpecific, organization,0);
-        System.out.println("Finished recommendation");
+        System.out.println("Finished recommendation from "+organization+" | "+new Date());
         return new ResponseEntity<>(ret, HttpStatus.CREATED);
     }
 
@@ -81,9 +82,9 @@ public class StakeholdersRecommenderController {
     @ApiOperation(value = "Set the mapping of effort points to hours for an specific project. The effort points go in a scale from 1 to 5.", notes = "")
     public ResponseEntity setEffort(@RequestBody SetEffortSchema eff, @ApiParam(value = "The project in which the effort mapping should be used.", example = "1", required = true) @RequestParam String project
             , @ApiParam(value = "The organization that is making the request.", example = "UPC", required = true) @RequestParam String organization) throws IOException {
-        System.out.println("Starting set effort");
+        System.out.println("Starting set effort from "+organization+" | "+new Date());
         effortCalc.setEffort(eff, project, organization);
-        System.out.println("Finished set effort");
+        System.out.println("Finished set effort from "+organization+" | "+new Date());
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
@@ -91,9 +92,9 @@ public class StakeholdersRecommenderController {
     @ApiOperation(value = "This endpoint generates a mapping of effort points into hours specific to the project specified, based in the historic information given. Each requirement sohuld contain the effort stated in a scale from 1 to 5, and the hours that have been needed to complete this requirement. Taking this into account, the service computes the average of hours needed per effort point.", notes = "")
     public ResponseEntity calculateEffort(@RequestBody EffortCalculatorSchema eff, @ApiParam(value = "The project in which the effort mapping will be used in future recommendations.", example = "1", required = true) @RequestParam String project
             , @ApiParam(value = "The organization that is making the request.", example = "UPC", required = true) @RequestParam String organization) throws IOException {
-        System.out.println("Starting computation of effort");
+        System.out.println("Starting computation of effort from "+organization+" | "+new Date());
         effortCalc.effortCalc(eff, project, organization);
-        System.out.println("Finished computation of effort");
+        System.out.println("Finished computation of effort from "+organization+" | "+new Date());
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
@@ -101,9 +102,9 @@ public class StakeholdersRecommenderController {
     @ApiOperation(value = "This endpoint is used to state that the user identified by REJECTED will again be considered as valid to the REQUIREMENT when the person USER ask for a recommendation over this requirement.", notes = "")
     public ResponseEntity undoRejection(@ApiParam(value = "Id of the person who was rejected.", example = "Not JohnDoe", required = true) @RequestParam("rejected") String rejected, @ApiParam(value = "Id of the person who made the initial rejection.", example = "JohnDoe", required = true) @RequestParam("user") String user, @ApiParam(value = "Id of the requirement from which the person REJECTED was rejected by the person USER.", example = "1", required = true) @RequestParam("requirement") String requirement
             , @ApiParam(value = "The organization that is making the request.", example = "UPC", required = true) @RequestParam String organization) {
-        System.out.println("Undoing rejection");
+        System.out.println("Undoing rejection from "+organization+" | "+new Date());
         stakeholdersRecommenderService.undo_recommend_reject(rejected, user, requirement, organization);
-        System.out.println("Finished rejection");
+        System.out.println("Finished rejection from "+organization+" | "+new Date());
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
@@ -112,9 +113,9 @@ public class StakeholdersRecommenderController {
     public ResponseEntity getPersonSkills(@ApiParam(value = "Id of the person.", example = "Not JohnDoe", required = true) @RequestParam("person") String person,
                                           @ApiParam(value = "The organization that is making the request.", example = "UPC", required = true) @RequestParam("organization") String organization,
                                           @ApiParam(value = "Maximum number of skills to be returned", example = "10", required = false) @RequestParam(value = "k", defaultValue = "-1", required = false) Integer k) {
-        System.out.println("Started get person skills");
+        System.out.println("Started get person skills from "+organization+" | "+new Date());
         List<Skill> skills = stakeholdersRecommenderService.getPersonSkills(person, organization, k);
-        System.out.println("Finished get person skills");
+        System.out.println("Finished get person skills from "+organization+" | "+new Date());
         return new ResponseEntity<>(skills, HttpStatus.OK);
     }
 
